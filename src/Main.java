121245057eb70509cf1f58b97666a4e768920e56
Zhang Kefei <zhangkf92@gmail.com>
2015-05-20 13:11:17 +0800

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,133 +1,127 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
+class TracelInfoCenter{
+	private Node[] cities;
+	public TracelInfoCenter(int cityNum) {
+		if(cityNum < 1)
+			throw new IllegalArgumentException("city number must be positive");
+		
+		cities = new Node[cityNum];
+		for(int i = 0; i < cityNum; i++)
+			cities[i] = new Node();
+		
+	}
+	
+	public void setAjacent(int city1, int city2){
+		checkCityNoRange(city1);
+		checkCityNoRange(city2);
+		if(city1 == city2)
+			throw new IllegalArgumentException("must be 2 different cities");
+		
+		city1 --;
+		city2 --;
+		cities[city1].addAdjcentNode(cities[city2]);
+		cities[city2].addAdjcentNode(cities[city1]);
+	}
+	
+	public void addFestiveCity(int cityNo){
+		checkCityNoRange(cityNo);
+		
+		cities[cityNo - 1].becomesFestiveCity();
+	}
+	
+	public int getMinDistToFestiveCity(int cityNo){
+		checkCityNoRange(cityNo);
+		
+		return cities[cityNo - 1].minDist;
+	}
+
+	private void checkCityNoRange(int cityNo){
+		if(cityNo < 1 || cityNo > cities.length)
+			throw new IllegalArgumentException(
+					"city No. should be in the range of 1 and " + cities.length);
+	}
+
+	class Node{
+		List<Node> adjacentNodes;
+		int minDist;
+//		int cityNo;
+		Node(){
+//			cityNo = no;
+			minDist = Integer.MAX_VALUE;
+			adjacentNodes = new LinkedList<Node>();
+		}
+		
+		/**
+		 * add adjacentNode of this node
+		 * assume one node will only be added once
+		 * @param adjcentNode
+		 */
+		void addAdjcentNode(Node adjcentNode){
+			adjacentNodes.add(adjcentNode);
+		}
+		
+		/**
+		 * invoked when this node becomes a festive city
+		 * updated minDist of all nodes needed
+		 */
+		void becomesFestiveCity(){
+			if(minDist == 0) return;	// already a festive city
+			
+			minDist = 0;
+			for(Node node: adjacentNodes)
+				node.refreshMinDistFromHere(1);
+		}
+		
+		void refreshMinDistFromHere(int dist){
+			if(dist >= minDist) return;
+			
+			minDist = dist;
+			for(Node node: adjacentNodes)
+				node.refreshMinDistFromHere(dist + 1);
+		}
+	}
+}
 
 public class Main {
-	private int[][] grid;
-	private int rowN, colN;
-	
-	public Main(int[][] grid, int rowN, int colN) {
-		this.grid = grid;
-		this.rowN = rowN;
-		this.colN = colN;
-	}
-
-	private int getBestScoreInColumn(int colIdx, int rowIdxStart, int rowIdxEnd){
-		if(grid[rowIdxStart][colIdx] == -1) return -1;	//start position blocked
+	public static void test(){
+		TracelInfoCenter tic = new TracelInfoCenter(5);
+		tic.setAjacent(1, 2);
+		tic.setAjacent(1, 3);
+		tic.setAjacent(3, 4);
+		tic.setAjacent(3, 5);
 		
-		int prevScore = (colIdx == 0) ? 0 : grid[rowIdxStart][colIdx - 1];
-		if(prevScore == -1) return -1;	//left of start position blocked
+		tic.addFestiveCity(1);
 		
-		//TODO:start position is the same as end, maybe not necessary
-		if(rowIdxStart == rowIdxEnd) return prevScore + grid[rowIdxStart][colIdx];
+		System.out.println(tic.getMinDistToFestiveCity(5));
+		System.out.println(tic.getMinDistToFestiveCity(3));
 		
-		int upScore = prevScore + grid[rowIdxStart][colIdx];
-		int downScore = upScore;
-		
-		//try upwards
-		int curRowIdx = rowIdxStart;
-		while(curRowIdx != rowIdxEnd){
-			if(curRowIdx == 0){
-				upScore = 0;
-				curRowIdx = rowN - 1;
-			} else curRowIdx --;
-			
-			if(grid[curRowIdx][colIdx] == -1){
-				upScore = -1;
-				break;
-			}
-			upScore += grid[curRowIdx][colIdx];
-		}
-		
-		//try downwards
-		curRowIdx = rowIdxStart;
-		while(curRowIdx != rowIdxEnd){
-			if(curRowIdx == rowN - 1){
-				downScore = 0;
-				curRowIdx = 0;
-			} else curRowIdx ++;
-			
-			if(grid[curRowIdx][colIdx] == -1){
-				downScore = -1;
-				break;
-			}
-			downScore += grid[curRowIdx][colIdx];
-		}
-		
-		return (upScore > downScore) ? upScore : downScore;
-	}
-	
-	private int getBestScoreByStopPoint(int rowIdx, int colIdx){
-		if(grid[rowIdx][colIdx] == -1) return -1;	//not possible to be a stop point
-		
-		int maxScore = -1;
-		for(int i = 0; i < rowN; i++){
-			int score = getBestScoreInColumn(colIdx, i, rowIdx);
-			if(score > maxScore) maxScore = score;
-		}
-		
-		return maxScore;
-	}
-	
-	private int solve(){
-		int[] colScores = new int[rowN];
-		for(int j = 0; j < colN; j++){
-			// get best score for each ending position in this column, -1 for impossible
-			for(int i = 0; i < rowN; i++)
-				colScores[i] = getBestScoreByStopPoint(i, j);
-			
-			for(int i = 0; i < rowN; i++)
-				grid[i][j] = colScores[i];
-		}
-		
-		int max = -1;
-		for(int i = 0; i < rowN; i++){
-			if(grid[i][colN - 1] > max) max = grid[i][colN - 1];
-		}
-		
-		return max;
-	}
-
-	public static void test(String[] args) {
-		int[][] grid = new int[][]{
-				new int[]{-1, 4, 5, 1}, 
-				new int[]{2, -1, 2, 4}, 
-				new int[]{3, 3, -1, 3}, 
-				new int[]{4, 2, 1, 2}
-				};
-		
-		System.out.println(new Main(grid, 4, 4).solve());
-		
-		grid = new int[][]{
-				new int[]{-1, 4, 5, 1}, 
-				new int[]{2, -1, 2, 4}, 
-				new int[]{3, 3, -1, -1}, 
-				new int[]{4, 2, 1, 2}
-				};
-		
-		System.out.println(new Main(grid, 4, 4).solve());
-		
-		grid = new int[][]{
-				new int[]{-1, 4, 5, 1}, 
-				new int[]{2, -1, 2, 4}, 
-				new int[]{3, 3, -1, 3}, 
-				new int[]{4, 2, -1, 2}
-				};
-		
-		System.out.println(new Main(grid, 4, 4).solve());
+		tic.addFestiveCity(3);
+		System.out.println(tic.getMinDistToFestiveCity(3));
+		System.out.println(tic.getMinDistToFestiveCity(4));
 	}
 
 	public static void main(String[] args) {
+//		test();
 		Scanner scanner = new Scanner(System.in);
-		int row = scanner.nextInt();
-		int col = scanner.nextInt();
-		int[][] grid = new int[row][col];
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				grid[i][j] = scanner.nextInt();
-			}
-		}
-		scanner.close();
 		
-		System.out.println(new Main(grid, row, col).solve());
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		
+		TracelInfoCenter tic = new TracelInfoCenter(n);
+		for(int i = 1; i < n; i++)
+			tic.setAjacent(scanner.nextInt(), scanner.nextInt());
+		
+		tic.addFestiveCity(1);
+		
+		for(int i = 0; i < m; i++){
+			if(scanner.nextInt() == 1)
+				tic.addFestiveCity(scanner.nextInt());
+			else
+				System.out.println(tic.getMinDistToFestiveCity(scanner.nextInt()));
+		}
 	}
+
 }
