cb09306fc1297f84dff54dee35d77612b07dc18f
ZhangKefei <zhangkf92@gmail.com>
2016-02-13 23:00:30 +0800

    commit

---
+++ src/cn/edu/zju/coffee/leetcode/no263and264/Solution.java
@@ -0,0 +1,48 @@
+package cn.edu.zju.coffee.leetcode.no263and264;
+
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num <= 0) return false;
+        if (num == 1) return true;
+
+        if (num % 2 == 0) return isUgly(num / 2);
+        if (num % 3 == 0) return isUgly(num / 3);
+        if (num % 5 == 0) return isUgly(num / 5);
+
+        return false;
+    }
+
+    public int nthUglyNumber(int n) {
+        long[] list = new long[n];
+        list[0] = 1;
+        for (int i = 1; i < n; i++) {
+            long min = Integer.MAX_VALUE;
+            for (int j = i-1; j >=0 ; j--) {
+                long cur = list[j] * 2l;
+                if (cur <= list[i-1]) break;
+                if (cur < min) min = cur;
+            }
+            for (int j = i-1; j >=0 ; j--) {
+                long cur = list[j] * 3l;
+                if (cur <= list[i-1]) break;
+                if (cur < min) min = cur;
+            }
+            for (int j = i-1; j >=0 ; j--) {
+                long cur = list[j] * 5l;
+                if (cur <= list[i-1]) break;
+                if (cur < min) min = cur;
+            }
+            list[i] = min;
+        }
+
+        return (int)list[n-1];
+    }
+
+    public static void main(String[] args) {
+		Solution sol = new Solution();
+        System.out.println(sol.nthUglyNumber(1));
+        System.out.println(sol.nthUglyNumber(2));
+        System.out.println(sol.nthUglyNumber(10));
+        System.out.println(sol.nthUglyNumber(1487));
+    }
+}
---
+++ src/cn/edu/zju/coffee/leetcode/no268/Solution.java
@@ -0,0 +1,18 @@
+package cn.edu.zju.coffee.leetcode.no268;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= (i+1);
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+
+    public static void main(String[] args) {
+		Solution sol = new Solution();
+        System.out.println(sol.missingNumber(new int[]{0,3,1}));
+    }
+}
